5ac0e6a12e0f4b44860c1d764fc8efe39bfe83d5
DFW2025 <81567386+DFW2025@users.noreply.github.com>
2021-07-30 10:50:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JAVASCRIPT
+
+
+    // getData()
+    //     .then(data => console.log(data))
+    //     .catch(reason => console.log("Data kon niet binnen worden gehaald " + " -- message: " + reason.message))
+
+// test()
+//     // .then(data => console.log(data))
+//     // .catch(reason => console.log("Data kon niet binnen worden gehaald " + " -- message: " + reason.message))
